2eaeec585e66668962cf69070540a8e07df94446
jd richards <jdrichardstech@gmail.com>
2024-11-08 15:05:18 -0500

    change file name

---
+++ dsa.js
@@ -0,0 +1,9 @@
+// Part 1: Create a function that finds the most frequent character
+// Input: string
+// Output: the character that appears most often
+// Example 1: "hello" => "l"
+// Example 2: "aabbcc" => "a" (if tie, return first one)
+
+function findMostFrequent(str) {
+    // Create your solution here
+  }
